3242908c26b63aee23670d03b7e026a3bc470bb0
Pratham-Galaxy <97865710+Pratham-Galaxy@users.noreply.github.com>
2026-07-10 14:47:04 +0530

    Updated content.ts

--- src/config/content.ts
+++ src/config/content.ts
@@ -144,7 +144,7 @@ export const MEMORY_CORNERS = [
     id: 'm1',
     corner: 'Turn 1',
     title: 'The First Race',
-    tag: 'F1',
+    tag: 'pizza',
     message: 'The first time you sat through a race with me. You asked what DRS was. I explained for 20 minutes. You said "cool" and asked for snacks. I knew then this was going to be a forever thing.',
     image: 'https://images.pexels.com/photos/2549298/pexels-photo-2549298.jpeg',
     placeholder: 'Photo: Replace with a screenshot of the first race we watched together.',
